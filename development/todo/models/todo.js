a9925c9ff0d1a5fca375218a13fe48d488ef5344
samk90 <kim.donghy@northeastern.edu>
2021-08-17 01:54:13 -0400

    created mongoose schema for todo list component

---
+++ development/todo/models/todo.js
@@ -0,0 +1,13 @@
+const mongoogse = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema ({
+  action: {
+    type: String,
+    required: [true, 'The todo text field is required']
+  }
+});
+
+const Todo = mongoose.model('todo', TodoSchema);
+
+module.exports = Todo;
